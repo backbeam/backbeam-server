af50af47d3686dfc72c1f3bf80bea1a6a8ad362d
Alberto Gimeno Brieba <gimenete@gmail.com>
2014-12-08 15:49:54 -0500

    Support for limit and offset

--- test/test-api-data.js
+++ test/test-api-data.js
@@ -10,8 +10,8 @@ var secret = 'bar'
 
 describe('Test API for data manipulation', function() {
 
-  var name = 'Item name'
-  var id
+  var name1 = 'Item name', name2 = 'Other item name'
+  var id1, id2
 
   before(function(done) {
     txain(function(callback) {
@@ -31,7 +31,7 @@ describe('Test API for data manipulation', function() {
         shared: shared,
         secret: secret,
         form: {
-          'set-name': name,
+          'set-name': name1,
         },
       })
       .end(function(err, res) {
@@ -40,12 +40,42 @@ describe('Test API for data manipulation', function() {
         assert.ok(res.body)
         assert.equal(res.body.status, 'Success')
         assert.ok(res.body.id)
-        id = res.body.id
+        id1 = res.body.id
         assert.ok(res.body.objects)
-        assert.ok(res.body.objects[id])
+        assert.ok(res.body.objects[id1])
 
-        var object = res.body.objects[id]
-        assert.equal(object['name#t'], name)
+        var object = res.body.objects[id1]
+        assert.equal(object['name#t'], name1)
+        assert.ok(object.created_at)
+        assert.ok(object.updated_at)
+        assert.equal(object['type'], 'item')
+        done()
+      })
+  })
+
+  it('should insert another object', function(done) {
+    request(app)
+      .api({
+        path: '/api/data/item',
+        method: 'post',
+        shared: shared,
+        secret: secret,
+        form: {
+          'set-name': name2,
+        },
+      })
+      .end(function(err, res) {
+        assert.ifError(err)
+        assert.equal(res.statusCode, 201)
+        assert.ok(res.body)
+        assert.equal(res.body.status, 'Success')
+        assert.ok(res.body.id)
+        id2 = res.body.id
+        assert.ok(res.body.objects)
+        assert.ok(res.body.objects[id2])
+
+        var object = res.body.objects[id2]
+        assert.equal(object['name#t'], name2)
         assert.ok(object.created_at)
         assert.ok(object.updated_at)
         assert.equal(object['type'], 'item')
@@ -56,7 +86,7 @@ describe('Test API for data manipulation', function() {
   it('should read an object', function(done) {
     request(app)
       .api({
-        path: '/api/data/item/'+id,
+        path: '/api/data/item/'+id1,
         method: 'get',
         shared: shared,
         secret: secret,
@@ -68,10 +98,10 @@ describe('Test API for data manipulation', function() {
         assert.equal(res.body.status, 'Success')
         assert.ok(res.body.id)
         assert.ok(res.body.objects)
-        assert.ok(res.body.objects[id])
+        assert.ok(res.body.objects[id1])
         
-        var object = res.body.objects[id]
-        assert.equal(object['name#t'], name)
+        var object = res.body.objects[id1]
+        assert.equal(object['name#t'], name1)
         assert.ok(object.created_at)
         assert.ok(object.updated_at)
         assert.equal(object['type'], 'item')
@@ -80,16 +110,16 @@ describe('Test API for data manipulation', function() {
   })
 
   it('should update an object', function(done) {
-    name = 'Other name'
+    name1 = 'Other name'
 
     request(app)
       .api({
-        path: '/api/data/item/'+id,
+        path: '/api/data/item/'+id1,
         method: 'put',
         shared: shared,
         secret: secret,
         form: {
-          'set-name': name,
+          'set-name': name1,
         },
       })
       .end(function(err, res) {
@@ -99,10 +129,10 @@ describe('Test API for data manipulation', function() {
         assert.equal(res.body.status, 'Success')
         assert.ok(res.body.id)
         assert.ok(res.body.objects)
-        assert.ok(res.body.objects[id])
+        assert.ok(res.body.objects[id1])
         
-        var object = res.body.objects[id]
-        assert.equal(object['name#t'], name)
+        var object = res.body.objects[id1]
+        assert.equal(object['name#t'], name1)
         assert.ok(object.created_at)
         assert.ok(object.updated_at)
         assert.equal(object['type'], 'item')
@@ -124,12 +154,13 @@ describe('Test API for data manipulation', function() {
         assert.ok(res.body)
         assert.equal(res.body.status, 'Success')
         assert.ok(res.body.ids)
-        assert.ok(res.body.ids.indexOf(id) >= 0)
+        assert.equal(res.body.ids.length, 2)
+        assert.ok(res.body.ids.indexOf(id1) >= 0)
         assert.ok(res.body.objects)
-        assert.ok(res.body.objects[id])
+        assert.ok(res.body.objects[id1])
         
-        var object = res.body.objects[id]
-        assert.equal(object['name#t'], name)
+        var object = res.body.objects[id1]
+        assert.equal(object['name#t'], name1)
         assert.ok(object.created_at)
         assert.ok(object.updated_at)
         assert.equal(object['type'], 'item')
@@ -137,10 +168,99 @@ describe('Test API for data manipulation', function() {
       })
   })
 
+  it('should perform a query with limit', function(done) {
+    request(app)
+      .api({
+        path: '/api/data/item/',
+        method: 'get',
+        shared: shared,
+        secret: secret,
+        qs: {
+          limit: 1,
+        }
+      })
+      .end(function(err, res) {
+        assert.ifError(err)
+        assert.equal(res.statusCode, 200)
+        assert.ok(res.body)
+        assert.equal(res.body.status, 'Success')
+        assert.ok(res.body.ids)
+        assert.equal(res.body.ids.length, 1)
+        assert.equal(res.body.ids[0], id1)
+        assert.ok(res.body.objects)
+        assert.ok(res.body.objects[id1])
+        
+        var object = res.body.objects[id1]
+        assert.equal(object['name#t'], name1)
+        assert.ok(object.created_at)
+        assert.ok(object.updated_at)
+        assert.equal(object['type'], 'item')
+
+        done()
+      })
+  })
+
+  it('should perform a query with limit and offset', function(done) {
+    request(app)
+      .api({
+        path: '/api/data/item/',
+        method: 'get',
+        shared: shared,
+        secret: secret,
+        qs: {
+          limit: 1,
+          offset: 1,
+        }
+      })
+      .end(function(err, res) {
+        assert.ifError(err)
+        assert.equal(res.statusCode, 200)
+        assert.ok(res.body)
+        assert.equal(res.body.status, 'Success')
+        assert.ok(res.body.ids)
+        assert.equal(res.body.ids.length, 1)
+        assert.equal(res.body.ids[0], id2)
+        assert.ok(res.body.objects)
+        assert.ok(res.body.objects[id2])
+        
+        var object = res.body.objects[id2]
+        assert.equal(object['name#t'], name2)
+        assert.ok(object.created_at)
+        assert.ok(object.updated_at)
+        assert.equal(object['type'], 'item')
+
+        done()
+      })
+  })
+
+  it('should test an empty query', function(done) {
+    request(app)
+      .api({
+        path: '/api/data/item/',
+        method: 'get',
+        shared: shared,
+        secret: secret,
+        qs: {
+          limit: 1,
+          offset: 2,
+        }
+      })
+      .end(function(err, res) {
+        assert.ifError(err)
+        assert.equal(res.statusCode, 200)
+        assert.ok(res.body)
+        assert.equal(res.body.status, 'Success')
+        assert.ok(res.body.ids)
+        assert.equal(res.body.ids.length, 0)
+
+        done()
+      })
+  })
+
   it('should remove an object', function(done) {
     request(app)
       .api({
-        path: '/api/data/item/'+id,
+        path: '/api/data/item/'+id1,
         method: 'del',
         shared: shared,
         secret: secret,
@@ -151,9 +271,9 @@ describe('Test API for data manipulation', function() {
         assert.ok(res.body)
         assert.equal(res.body.status, 'Success')
         assert.ok(res.body.id)
-        assert.equal(res.body.id, id)
-        var object = res.body.objects[id]
-        assert.equal(object['name#t'], name)
+        assert.equal(res.body.id, id1)
+        var object = res.body.objects[id1]
+        assert.equal(object['name#t'], name1)
         assert.ok(object.created_at)
         assert.ok(object.updated_at)
         assert.equal(object['type'], 'item')
