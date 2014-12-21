adb2c9f165329e0e08947bc7f2dfb6413279e7ef
Alberto Gimeno Brieba <gimenete@gmail.com>
2014-12-21 18:49:31 -0500

    Query by nearest coordinates

--- test/test-api-data-query-geo.js
+++ test/test-api-data-query-geo.js
@@ -4,6 +4,7 @@ var txain  = require('txain')
 var _ = require('underscore')
 var request = utils.request
 var app = require('./app')()
+var geo = require('../lib/util/geo')
 
 var shared = 'foo'
 var secret = 'bar'
@@ -126,7 +127,7 @@ describe('Test API for geo queries', function() {
           callback()
         })
     })
-    // remove some locaations
+    // remove some locations
     .then(function(callback) {
       var toremove = _.union(toremove, expected.splice(0, 3))
       assert.ok(expected.length > 0)
@@ -200,6 +201,142 @@ describe('Test API for geo queries', function() {
     })
     .then(function(callback) {
       requestBounding(expected, callback)
+    })
+    .end(done)
+  })
+
+  it('should query using nearest', function(done) {
+    var query = '', params = []
+    var lat = 40.0495297, lon = -3.985908
+    var point = {
+      lat: lat,
+      lon: lon,
+    }
+
+    // calculate result manually
+    records.forEach(function(record) {
+      record.distance = geo.distance(point, {
+        lat: record.lat,
+        lon: record.lon,
+      })
+    })
+    var expected = _.sortBy(records, function(record) {
+      return record.distance
+    })
+    assert.ok(expected.length > 0)
+
+    // ignore 10 locations
+    var limit = expected.length - 10
+    expected.splice(limit)
+
+    function requestNearest(expected, callback) {
+      request(app)
+        .api({
+          path: '/api/data/item/near/location',
+          method: 'get',
+          shared: shared,
+          secret: secret,
+          qs: {
+            q: 'sort by created_at desc',
+            lat: lat,
+            lon: lon,
+            limit: expected.length,
+          },
+        })
+        .end(function(err, res) {
+          assert.ifError(err)
+          assert.equal(res.statusCode, 200, res.text)
+          assert.ok(res.body)
+          assert.ok(res.body.ids)
+          assert.ok(res.body.objects)
+          var ids = res.body.ids
+          // only the first half of the ids are tested because the db
+          // does not do the same calculations
+          for (var i = 0; i < ids.length / 2; i++) {
+            assert.equal(ids[i], expected[i].id)
+          }
+          callback()
+        })
+    }
+
+    txain([3, 10, 20, 30])
+    .each(function(limit, callback) {
+      requestNearest(expected.slice(0, limit), callback)
+    })
+    // remove some locations
+    .then(function(callback) {
+      var toremove = _.union(toremove, expected.splice(0, 3))
+      assert.ok(expected.length > 0)
+      callback(null, toremove)
+    })
+    .each(function(record, callback) {
+      request(app)
+        .api({
+          path: '/api/data/item/'+record.id,
+          method: 'del',
+          shared: shared,
+          secret: secret,
+        })
+        .end(function(err, res) {
+          assert.ifError(err)
+          assert.equal(res.statusCode, 200, res.text)
+          callback()
+        })
+    })
+    .then(function(callback) {
+      requestNearest(expected, callback)
+    })
+    // change some locations
+    .then(function(callback) {
+      var tochange = expected.splice(0, 2)
+      assert.ok(tochange.length > 0)
+      callback(null, tochange)
+    })
+    .each(function(record, callback) {
+      request(app)
+        .api({
+          path: '/api/data/item/'+record.id,
+          method: 'put',
+          shared: shared,
+          secret: secret,
+          form: {
+            'set-location': 'Address for '+record.name, // no lat/lon
+          },
+        })
+        .end(function(err, res) {
+          assert.ifError(err)
+          assert.equal(res.statusCode, 200, res.text)
+          callback()
+        })
+    })
+    .then(function(callback) {
+      requestNearest(expected, callback)
+    })
+    // delete some locations
+    .then(function(callback) {
+      var tochange = expected.splice(0, 2)
+      assert.ok(tochange.length > 0)
+      callback(null, tochange)
+    })
+    .each(function(record, callback) {
+      request(app)
+        .api({
+          path: '/api/data/item/'+record.id,
+          method: 'put',
+          shared: shared,
+          secret: secret,
+          form: {
+            'del-location': 1,
+          },
+        })
+        .end(function(err, res) {
+          assert.ifError(err)
+          assert.equal(res.statusCode, 200, res.text)
+          callback()
+        })
+    })
+    .then(function(callback) {
+      requestNearest(expected, callback)
     })
     .end(done)
   })
